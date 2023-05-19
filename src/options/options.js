e322c15c926b1921e216cf894289358a13525026
Svetlana Vrublevskaya <vsa@scand.com>
2023-05-19 17:42:29 +0300

    #4: Adds script for displaying selected item from sidebar menu with relevant content part

--- src/options/options.js
+++ src/options/options.js
@@ -1,3 +1,35 @@
 document.querySelectorAll("[data-locale]").forEach((elem) => {
     elem.innerText = browser.i18n.getMessage(elem.dataset.locale);
 });
+
+function openSection(event, sectionName) {
+    let sectionContent = document.getElementsByClassName("section-content");
+    for (let i = 0; i < sectionContent.length; i++) {
+        sectionContent[i].style.display = "none";
+        sectionContent[i].className = sectionContent[i].className.replace(" active", "");
+    }
+
+    let sidebarLinks = document.getElementsByClassName("sidebar-link");
+    for (let i = 0; i < sidebarLinks.length; i++) {
+        sidebarLinks[i].className = sidebarLinks[i].className.replace(" active", "");
+    }
+
+    document.getElementById(sectionName).style.display = "block";
+    event.currentTarget.className += " active";
+}
+
+const sideBarItemsArray = document.querySelectorAll('.sidebar-link');
+
+// TODO
+sideBarItemsArray.forEach(function(item) {
+    item.addEventListener('click', {
+        handleEvent(event) {
+            openSection(event, event.target.innerHTML);
+            console.log('1');
+        }
+    });
+
+    // item.addEventListener('click',
+    //     openSection(event, event.target.innerHTML)
+    // );
+})
