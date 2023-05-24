721951cbf414e9bbe33f3dbcf71506c1efa5feda
Svetlana Vrublevskaya <vsa@scand.com>
2023-05-24 14:59:27 +0300

    #4: Changes js-files structure, start working with local.storage, adds functionality to Clear button

--- src/options/options.js
+++ src/options/options.js
@@ -4,6 +4,34 @@ document.querySelectorAll("[data-locale]").forEach((elem) => {
 
 const radioButtons = document.querySelectorAll('input[name="tab-group-1"]');
 const optionsTitle = document.getElementById('options-title');
+const shortcutClearBtnArray = document.querySelectorAll('.clear-button');
+/*
+Default settings. Initialize storage to these values.
+*/
+let defaultShortCuts = {
+    id: '_execute_browser_action',
+    shortcut: 'Ctrl+Alt+U'
+}
+
+/*
+Generic error logger.
+*/
+function onError(e) {
+    console.error(e);
+}
+
+/*
+On startup, check whether we have stored settings.
+If we don't, then store the default settings.
+*/
+function checkStoredSettings(storedSettings) {
+    if (!storedSettings.defaultShortCuts) {
+        browser.storage.local.set({defaultShortCuts});
+    }
+}
+
+const gettingStoredSettings = browser.storage.local.get();
+gettingStoredSettings.then(checkStoredSettings, onError);
 
 radioButtons.forEach((item) => {
     item.addEventListener('click', () => {
@@ -14,4 +42,12 @@ radioButtons.forEach((item) => {
             }
         }
     })
-})
+});
+
+shortcutClearBtnArray.forEach((item) => {
+    item.addEventListener('click', () => {
+        if (item.previousElementSibling.value) {
+            item.previousElementSibling.value = ''
+        }
+    });
+});
