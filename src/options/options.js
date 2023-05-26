e342ad092f86619f34f6a3cd1df3e20eacd44b65
Svetlana Vrublevskaya <vsa@scand.com>
2023-05-26 17:23:53 +0300

    #4: Restructuring handlers for Clear and Reset buttons

--- src/options/options.js
+++ src/options/options.js
@@ -3,25 +3,26 @@ document.querySelectorAll("[data-locale]").forEach((elem) => {
 });
 
 const radioButtons = document.querySelectorAll('input[name="tab-group-1"]');
-const optionsTitle = document.getElementById('options-title');
-const shortcutClearBtnArray = document.querySelectorAll('.clear-button');
-const shortcutResetBtnArray = document.querySelectorAll('.reset-button');
-const inputsShortcutsArr = document.querySelectorAll('.shortcuts-input');
+const optionsTitle = document.getElementById("options-title");
+const shortcutClearBtnArray = document.querySelectorAll(".clear-button");
+const shortcutResetBtnArray = document.querySelectorAll(".reset-button");
+const inputsShortcutsArr = document.querySelectorAll(".shortcuts-input");
 /*
 Default settings. Initialize storage to these values.
 */
-
 const defaultShortCutsArr = [
     {
-        id: '_execute_browser_action',
-        shortcut: 'Ctrl+Alt+U'
+        id: "_execute_browser_action",
+        shortcut: "Ctrl+Alt+L"
     }
 ]
 
-let storedShortCutsArr = {
-    id: '_execute_browser_action',
-    shortcut: 'Ctrl+Alt+U'
-}
+let storedShortCutsArr = [
+    {
+        id: "",
+        shortcut: ""
+    }
+]
 
 /*
 Generic error logger.
@@ -38,13 +39,23 @@ function checkStoredSettings(storedSettings) {
     if (!storedSettings.storedShortCutsArr) {
         browser.storage.local.set({storedShortCutsArr});
     }
+    setShortCutsOnLoadPage();
+}
+
+function setShortCutsOnLoadPage() {
+    inputsShortcutsArr.forEach((item) => {
+        let getShortcutElement = storedShortCutsArr.filter((el) => el.id === item.id);
+        let getDefaultShortCuts = defaultShortCutsArr.filter((el) => el.id === item.id)
+        getShortcutElement.length ? (item.value = getShortcutElement.shortcut) : (item.value = getDefaultShortCuts[0].shortcut);
+        browser.commands.update({ name: item.id, shortcut: getDefaultShortCuts[0].shortcut });
+    })
 }
 
 const gettingStoredSettings = browser.storage.local.get();
 gettingStoredSettings.then(checkStoredSettings, onError);
 
 radioButtons.forEach((item) => {
-    item.addEventListener('click', () => {
+    item.addEventListener("click", () => {
         for (const radioButton of radioButtons) {
             if (radioButton.checked) {
                 optionsTitle.innerText = radioButton.value;
@@ -54,31 +65,40 @@ radioButtons.forEach((item) => {
     })
 });
 
+// Clear button handler
 shortcutClearBtnArray.forEach((item) => {
-    item.addEventListener('click', () => {
-        if (item.previousElementSibling.value) {
-            item.previousElementSibling.value = ''
-        }
+    item.addEventListener("click", () => {
+        item.previousElementSibling.value = ""
+        storedShortCutsArr.filter((el) => el.id === item.id);
+        storedShortCutsArr[0].id = item.previousElementSibling.id;
+        storedShortCutsArr[0].shortcut = "";
+        browser.commands.update({ name: "_execute_browser_action", shortcut: "" });
+        browser.storage.local.set({storedShortCutsArr});
     });
 });
 
+// Reset button handler
 shortcutResetBtnArray.forEach((item) => {
-    item.addEventListener('click',() => {
+    item.addEventListener("click",() => {
         let currentInput = item.parentNode.firstChild.nextSibling;
         let getDefaultShortCutsById = defaultShortCutsArr.filter((item) => item.id === currentInput.id);
         currentInput.value = getDefaultShortCutsById[0].shortcut;
+        storedShortCutsArr.filter((el) => el.id === currentInput.id);
+        storedShortCutsArr[0].shortcut = currentInput.value;
+        storedShortCutsArr[0].id = currentInput.id;
+        browser.commands.update({ name: "_execute_browser_action", shortcut: currentInput.value });
+        browser.storage.local.set({storedShortCutsArr});
     })
 })
 
 // shortcuts setup
 inputsShortcutsArr.forEach((item) => {
-    item.addEventListener('keydown', (e) => {
-        console.log('keydown e', e);
-        console.log('e.target', e.target);
+    item.addEventListener("keydown", (e) => {
+        console.log("keydown e", e);
+        console.log("e.target", e.target);
         handleKeyDown(e)
     })
 })
-
 
 const normalizeKey = (key, keyCode) => {
     const alphabet = /^([a-z]|[A-Z])$/;
@@ -109,6 +129,7 @@ const normalizeKey = (key, keyCode) => {
     return "";
 };
 const isMac = getOS() == "Mac OS";
+
 function handleKeyDown(e) {
     if (e.repeat) return;
     if (e.key == "Tab") {
@@ -134,41 +155,38 @@ function handleKeyDown(e) {
         e.metaKey && isMac ? "Command+" : ""
     }${e.altKey ? "Alt+" : ""}${e.shiftKey ? "Shift+" : ""}${normalizedKey}`;
 
-    // this.setState({ error: error, value: value || "" });
+    e.target.value = value || "";
     const isValidShortcut = value != "" && error == "";
-    if (isValidShortcut) updateShortcut(value);
-    e.target.value = value;
-}
 
-async function updateShortcut(shortcut) {
-    console.log('shortcut', shortcut)
-    try {
-        await browser.commands.update({ name: "_execute_browser_action", shortcut: shortcut });
-        // this.setState({ shortcut: shortcut || "" });
-    } catch (e) {
-        // this.setState({ error: "invalidShortcutMessage" });
+    if (isValidShortcut) {
+        storedShortCutsArr.filter((el) => el.id === e.target.id);
+        storedShortCutsArr[0].shortcut = e.target.value;
+        storedShortCutsArr[0].id = e.target.id;
+        browser.commands.update({ name: e.target.id, shortcut: value });
+        browser.storage.local.set({storedShortCutsArr});
+    } else {
+        console.log('INVALID SHORTCUTS');
     }
 }
 
-
 function getOS() {
-    var userAgent = window.navigator.userAgent,
+    let userAgent = window.navigator.userAgent,
         platform = window.navigator?.userAgentData?.platform || window.navigator.platform,
-        macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
-        windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
-        iosPlatforms = ['iPhone', 'iPad', 'iPod'],
+        macosPlatforms = ["Macintosh", "MacIntel", "MacPPC", "Mac68K"],
+        windowsPlatforms = ["Win32", "Win64", "Windows", "WinCE"],
+        iosPlatforms = ["iPhone", "iPad", "iPod"],
         os = null;
 
     if (macosPlatforms.indexOf(platform) !== -1) {
-        os = 'Mac OS';
+        os = "Mac OS";
     } else if (iosPlatforms.indexOf(platform) !== -1) {
-        os = 'iOS';
+        os = "iOS";
     } else if (windowsPlatforms.indexOf(platform) !== -1) {
-        os = 'Windows';
+        os = "Windows";
     } else if (/Android/.test(userAgent)) {
-        os = 'Android';
+        os = "Android";
     } else if (/Linux/.test(platform)) {
-        os = 'Linux';
+        os = "Linux";
     }
 
     return os;
