b9941e743939de0270f7add46485426a98519f76
Svetlana Vrublevskaya <vsa@scand.com>
2023-07-05 19:01:44 +0300

    #5: Implements opening containers with default values of delays.

--- src/background.js
+++ src/background.js
@@ -1,20 +1,30 @@
 /*
 Open current tab in available Containers.
 */
-
 async function openCurrentTabInAvailableContainers(
   currentTabUrl,
   currentCookieStoreId,
+  numberContainersInGroup,
+  delayToOpenBetweenGroupsMSeconds,
+  delayToOpenBetweenContainersMSeconds
 ) {
   const containers = await browser.contextualIdentities.query({});
-  containers.forEach((container) => {
-    if (container.cookieStoreId !== currentCookieStoreId) {
-      browser.tabs.create({
-        cookieStoreId: container.cookieStoreId,
-        url: currentTabUrl,
-      });
-    }
-  });
+
+  for (let i = 0; i < containers.length; i++) {
+    let isNewGroup = (i !== 0 && i % 3 === 0) ? true : false;
+    let delayToOpenContainers = isNewGroup ? i * delayToOpenBetweenContainersMSeconds + delayToOpenBetweenGroupsMSeconds : i * delayToOpenBetweenContainersMSeconds;
+    console.log({'delayToOpenContainers': delayToOpenContainers});
+    (function (index) {
+      setTimeout(function () {
+        if (containers[i].cookieStoreId !== currentCookieStoreId) {
+          browser.tabs.create({
+            cookieStoreId: containers[i].cookieStoreId,
+            url: currentTabUrl,
+          });
+        }
+      }, delayToOpenContainers);
+    })(i);
+  }
 }
 
 /*
@@ -31,7 +41,7 @@ The click event listener, where we perform the appropriate action
 when extension menu item ('Open in available Containers') was clicked.
 */
 browser.menus.onClicked.addListener((info, tab) => {
-  openCurrentTabInAvailableContainers(tab.url, tab.cookieStoreId);
+  openCurrentTabInAvailableContainers(tab.url, tab.cookieStoreId, 3, 500, 1000);
 });
 
 /*
@@ -39,5 +49,5 @@ The click event listener, where we perform the appropriate action
 when extension icon was clicked.
 */
 browser.browserAction.onClicked.addListener((tab, OnClickData) => {
-  openCurrentTabInAvailableContainers(tab.url, tab.cookieStoreId);
+  openCurrentTabInAvailableContainers(tab.url, tab.cookieStoreId, 3, 500, 1000);
 });
